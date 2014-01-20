3eb0f7417e8d9fcd02ee644bc22195ad6c3dbbbb
Justin <jhugon@cern.fake>
2014-01-20 15:06:44 -0500

    added fermion and non-arrow lines

--- feynmanGraphSVG.py
+++ feynmanGraphSVG.py
@@ -4,7 +4,7 @@ import numpy
 import svgwrite
 from math import sqrt,pi
 
-def sinWave(path,p1,p2,capped1=True,capped2=True,amp=5,period=50,width=10):
+def wavyLine(path,p1,p2,capped1=True,capped2=True,amp=5,period=50,width=10):
   p1 = numpy.array(p1)
   p2 = numpy.array(p2)
   propVec = p2-p1
@@ -101,18 +101,80 @@ def spiralLine(path,p1,p2,capped1=True,capped2=True,amp=60,period=50,width=10):
     path.push(['m']+list(normedPerpVec*width/4.))
   return path
 
+def straightLine(path,p1,p2,capped1=True,capped2=True,width=10):
+  p1 = numpy.array(p1)
+  p2 = numpy.array(p2)
+  propVec = p2-p1
+  distance = sqrt(numpy.dot(propVec,propVec))
+  normedPropVec = propVec/distance
+  normedPerpVec = numpy.array([-normedPropVec[1],normedPropVec[0]])
+  path.push(['M']+list(p1))
+  if capped1:
+    path.push(['l']+list(normedPerpVec*width/2.))
+  else:
+    path.push(['m']+list(normedPerpVec*width/2.))
+  path.push(['l']+list(propVec))
+  if capped2:
+    path.push(['l']+list(-normedPerpVec*width))
+  else:
+    path.push(['m']+list(-normedPerpVec*width))
+  propVec *= -1
+  path.push(['l']+list(propVec))
+  if capped1:
+    path.push(['l']+list(normedPerpVec*width/2.))
+  else:
+    path.push(['m']+list(normedPerpVec*width/2.))
+  return path
+
+def straightLineArrow(path,p1,p2,capped1=True,capped2=True,forward=True,width=10,arrowLength=30,arrowWidth=None):
+  if arrowWidth == None:
+    arrowWidth = width
+  p1 = numpy.array(p1)
+  p2 = numpy.array(p2)
+  propVec = p2-p1
+  distance = sqrt(numpy.dot(propVec,propVec))
+  normedPropVec = propVec/distance
+  normedPerpVec = numpy.array([-normedPropVec[1],normedPropVec[0]])
+  path.push(['M']+list(p1))
+  if capped1:
+    path.push(['l']+list(normedPerpVec*width/2.))
+  else:
+    path.push(['m']+list(normedPerpVec*width/2.))
+  path.push(['l']+list(normedPropVec*(distance/2.-arrowLength/2.)))
+  if forward:
+    path.push(['l']+list(normedPerpVec*arrowWidth))
+    path.push(['l']+list(normedPropVec*arrowLength-normedPerpVec*arrowWidth))
+  else:
+    path.push(['l']+list(normedPropVec*arrowLength+normedPerpVec*arrowWidth))
+    path.push(['l']+list(-normedPerpVec*arrowWidth))
+  path.push(['l']+list(normedPropVec*(distance/2.-arrowLength/2.)))
+  if capped2:
+    path.push(['l']+list(-normedPerpVec*width))
+  else:
+    path.push(['m']+list(-normedPerpVec*width))
+  # Back the other way
+  path.push(['l']+list(-normedPropVec*(distance/2.-arrowLength/2.)))
+  if forward:
+    path.push(['l']+list(-normedPropVec*arrowLength-normedPerpVec*arrowWidth))
+    path.push(['l']+list(normedPerpVec*arrowWidth))
+  else:
+    path.push(['l']+list(-normedPerpVec*arrowWidth))
+    path.push(['l']+list(-normedPropVec*arrowLength+normedPerpVec*arrowWidth))
+  path.push(['l']+list(-normedPropVec*(distance/2.-arrowLength/2.)))
+  if capped1:
+    path.push(['l']+list(normedPerpVec*width/2.))
+  else:
+    path.push(['m']+list(normedPerpVec*width/2.))
+  return path
+
 color = svgwrite.rgb(0, 0, 255)
 width = "1mm"
 
 dwg = svgwrite.Drawing('test.svg',size=("181mm","181mm"))
 path = dwg.path(None,stroke=color,stroke_width=width,fill="none")
-path.push(['M',100,100])
-path.push(['l',100,000])
-path.push(['M',100,300])
-path.push(['l',400,000])
-#path.push(['M',100,100])
-#path.push(['l',000,100])
-sinWave(path,(100,200),(500,200))  # This thing isn't drawing the correct length
-spiralLine(path,(100,400),(500,700))
+wavyLine(path,(100,200),(500,200))  # This thing isn't drawing the correct length
+straightLine(path,(100,100),(500,150))
+straightLineArrow(path,(100,50),(500,550),forward=False)
+spiralLine(path,(100,400),(500,400))
 dwg.add(path)
 dwg.save()
